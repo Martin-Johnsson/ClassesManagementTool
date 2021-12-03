6d8fffe515320df0c9002d93c4a703a9451847da
Martin Johnsson <maartinjohnsson@gmail.com>
2021-12-03 10:25:54 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let profileCard = document.createElement("div");
+profileCard.style.border = "2px solid black";
+//profileCard.style.margin = "0 0 5px 0";
+
+let name = document.createElement("p")
+name.textContent = `Name: ${}
